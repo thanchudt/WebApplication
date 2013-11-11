55afdee416bf4d17333ce0111db12dba95ab981c
thanchudt <dangle110782@gmail.com>
2013-11-11 14:24:03 +0700

    Create project

---
+++ MvcApplication1/MvcApplication1/Areas/Shop/Controllers/BeurreAndSelController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Areas.Shop.Controllers
+{
+    public class BeurreAndSelController : Controller
+    {
+        //
+        // GET: /Shop/BeurreAndSel/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
