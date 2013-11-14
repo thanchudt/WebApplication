bec44af9050ee38a95f6b65811bdc4aa6452d5f5
thanchudt <dangle110782@gmail.com>
2013-11-14 15:34:29 +0700

    fsdfsdfsdf

---
+++ MvcApplication1/MvcApplication1/Areas/Shop/Controllers/StoryController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Areas.Shop.Controllers
+{
+    public class StoryController : Controller
+    {
+        //
+        // GET: /Shop/Story/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
